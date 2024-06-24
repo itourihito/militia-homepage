a4ca2e9ad08c2dcab332f9221f561feba2ebe7c3
sui tukimi <sui@suinoMacBook-Pro.local>
2024-06-24 12:00:58 +0900

    kaigyou

--- app.js
+++ app.js
@@ -29,17 +29,17 @@ const transporter = nodemailer.createTransport({
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.engine('hbs', engine({
+const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: '.hbs',
     helpers: {
-        breaklines: function (text) {
-            let escapedText = text.replace(/</g, "&lt;").replace(/>/g, "&gt;"); // XSS対策
-            return escapedText.replace(/(\r\n|\n|\r)/gm, '<br>');
-        }
+      breaklines: function (text) {
+        let escapedText = Handlebars.Utils.escapeExpression(text); // XSS対策
+        return new Handlebars.SafeString(escapedText.replace(/(\r\n|\n|\r)/gm, '<br>'));
+      }
     }
-}));
-
+  });
+app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
